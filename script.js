60207b92e36311335f226f43de8536779d530b05
Prodi-G <anesu96@gmail.com>
2021-07-26 13:57:16 -0400

    syntax

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principalvar").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal*years*rate/100;
